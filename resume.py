0dcedef70417258bb7ab9a3e2915ca513b560f8b
Tony Velardi <avelardi@system.local>
2016-02-16 11:52:26 -0500

    Initial Commit
    First Push

---
+++ resume.py
@@ -0,0 +1,27 @@
+#my-resume
+#v2.0
+
+from flask import Flask, render_template
+import yaml
+import os
+app = Flask(__name__)
+
+##for testing and stuff
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+def get_file(filename):
+    try:
+        src = os.path.join(os.path.abspath(os.path.dirname(__file__)) + "/static/assets", filename)
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+infofile = get_file("resume.yaml")
+info = yaml.load(infofile)
+
+@app.route('/')
+def resume():
+    return render_template('index.html',info=info)
+
+if __name__ == '__main__':
+    app.run()
